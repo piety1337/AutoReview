a5553205d911efef0e3dc15ec564d51e5ffcef05
piety1337 <125813721+piety1337@users.noreply.github.com>
2023-08-10 22:56:58 +0100

    Update WinServ2022.py

--- WinServ2022.py
+++ WinServ2022.py
@@ -4,50 +4,83 @@ import time
 
 init(autoreset=True)
 
+passed_checks = 0
+failed_checks = 0
+error_checks = 0
+
 def run():
-    print(Fore.GREEN + "======== Executing CIS Microsoft Windows Server 2022 Benchmark v2.0.0 script ========")
+    print(Fore.GREEN + "            ======== Executing CIS Microsoft Windows Server 2022 Benchmark v2.0.0 script ========")
     time.sleep(2)
 
+    print()
 def account_policies():   
-    print(Fore.BLUE + "======== 1. Account Policies ========")
+    print(Fore.BLUE + "                                 ======== 1. Account Policies ========")
 
 def password_policy_check():
-    time.sleep(2)
+    global passed_checks, failed_checks, error_checks
+    time.sleep(1)
     
-    print(Fore.CYAN + "======== 1.1 Password Policies ========")
+    print()
+    print(Fore.CYAN + "                                 ======== 1.1 Password Policies ========")
+    print()
     time.sleep(1)
     
     password_history = "net accounts | Select-String 'Password history' | ForEach-Object { $_ -replace '\D+(\d+)','$1'}"
     password_history_result = subprocess.run(['powershell', '-command', password_history], shell=True, capture_output=True, text=True)
     if password_history_result.returncode == 0:
         output = password_history_result.stdout.strip()
-        password_history_value = int(output)
-        if password_history_value >= 24:
-            print(Fore.GREEN + "[+] 1.1.1 PASSED: Length of password history value:", password_history_value)  
+        if output.isdigit():
+            password_history_value = int(output)
+            if password_history_value >= 24:
+                print(Fore.GREEN + "[+] 1.1.1 PASSED: Length of password history value:", password_history_value)
+                passed_checks += 1 
+            else:
+                print(Fore.RED + "[-] 1.1.1 FAILED: Length of password history value:", password_history_value)
+                print(Fore.RED + "   [!] Password history length should be 24 or more.")
+                failed_checks += 1
         else:
-            print(Fore.RED + "[-] 1.1.1 FAILED: Length of password history value:", password_history_value)
-    if password_history_value < 24:
-        print(Fore.RED + "   [!] Password history length should be 24 or more.")
+            print(Fore.RED + "[-] Error: Length of password history value is not valid.")
+            error_checks += 1
     else:
-        return f"Command failed. Error output: {password_history_result.stderr}"
+        print(Fore.RED + "=====================================================================")
+        print(Fore.RED + f"1.1.1 Command failed. Error output: {password_history_result.stderr}")
+        print(Fore.RED + "=====================================================================")
+        error_checks += 1
+    
     time.sleep(1)
     
     print()
     
-    max_password_age = "net accounts | Select-String 'Maximum password age' | ForEach-Object { $_ -replace '\D+(\d+)','$1'}"
+    max_password_age = "net accounts | Select-String 'Maximum password age' | ForEach-Object { $_ -replace '\\D+(\\d+)','$1'}"
     max_password_age_result = subprocess.run(['powershell', '-command', max_password_age], shell=True, capture_output=True, text=True)
     if max_password_age_result.returncode == 0:
         output = max_password_age_result.stdout.strip()
-        max_password_age_value = int(output)
-        if max_password_age_value <= 365 and max_password_age_value != 0:
-            print(Fore.GREEN + "[+] 1.1.2 PASSED: Maximum password age value:", max_password_age_value)
+        if output.isdigit():
+            max_password_age_value = int(output)
+            if max_password_age_value <= 365 and max_password_age_value != 0:
+                print(Fore.GREEN + "[+] 1.1.2 PASSED: Maximum password age value:", max_password_age_value)
+                passed_checks += 1
+            else:
+                print(Fore.RED + "[-] 1.1.2 FAILED: Maximum password age value:", max_password_age_value)
+                print(Fore.RED + "   [!] Maximum password age should be set to 365 or fewer days, but not 0.")
+                failed_checks += 1
         else:
-            print(Fore.RED + "[-] 1.1.2 FAILED: Maximum password age value:", max_password_age_value)
-    if max_password_age_value == 0 or max_password_age_value > 365:
-        print(Fore.RED + "   [!] Maximum password age should be set to 365 or fewer days, but not 0")
+            print(Fore.RED + "[-] Error: Maximum password age value is not valid.")
+            error_checks += 1
     else:
-        return f"Command failed. Error output: {password_history_result.stderr}"
+        print(Fore.RED + "=====================================================================")
+        print(Fore.RED + f"1.1.2 Command failed. Error output: {max_password_age_result.stderr}")
+        print(Fore.RED + "=====================================================================")
+        error_checks += 1
 
-def spliter():
-    spliter = Fore.MAGENTA + "================================================"
-    print(spliter)
+
+    print()
+    
+    print(Fore.GREEN + f"Checks Passed: {passed_checks}")
+    print(Fore.RED + f"Checks Failed: {failed_checks}")
+    print(Fore.RED + f"Checks Errored: {error_checks}")
+
+
+def splitter():
+    splitter = Fore.MAGENTA + "================================================"
+    print(splitter)
